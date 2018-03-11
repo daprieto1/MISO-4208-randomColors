dcdabb1e05e74d91eeffa53cbd623e0d277b9b18
daprieto1 <da.prieto1@uniandes.edu.co>
2018-03-11 01:28:17 -0500

    end app

---
+++ script.js
@@ -0,0 +1,63 @@
+
+var numRandomColors = 5;
+
+function randomPalette() {
+    var gradeInterval = 1 / numRandomColors;
+
+    var r, g, b;
+    r = getRandomInt(0, 255);
+    g = getRandomInt(0, 255);
+    b = getRandomInt(0, 255);
+
+    console.log(`Random RGB: R = ${r} G = ${g} B = ${b}`);
+    var hsl = rgbToHsl(r, g, b);
+    var randomColors = [];
+
+    for (var i = 0; i < numRandomColors; i++) {
+        hsl[0] = getNextHue(hsl[0], gradeInterval);
+        var rgb = hslToRgb(hsl[0], hsl[1], hsl[2]);
+        var hex = rgbToHex(rgb[0], rgb[1], rgb[2]).toUpperCase();
+        console.log(`${i + 1}   RGB: R = ${rgb[0]} G = ${rgb[1]} B = ${rgb[2]}`);
+        console.log(`   HEX: ${hex}`);
+        randomColors.push(hex);
+    }
+
+    generateRules(randomColors);
+}
+
+function generateRules(randomColors) {
+    var rules = [
+        `.website-background{ color: ${randomColors[0]};}`,
+        `.element-text{ color: ${randomColors[1]};}`,
+        `.element-border{ border-color: ${randomColors[2]};}`,
+        `.element-background{ background-color: ${randomColors[3]};}`,
+        `.header{ color: ${randomColors[4]};}`
+    ];
+    for (var i = 0; i < numRandomColors; i++) {
+        $(`#color${i + 1}`).css('background-color', randomColors[i]);
+    }
+    $("textarea#css-rules").val(rules.join('\r\n\n'));
+}
+
+function cleanPalette() {
+    var randomColors = [
+        '#FFFFFF',
+        '#FFFFFF',
+        '#FFFFFF',
+        '#FFFFFF',
+        '#FFFFFF'
+    ];
+    generateRules(randomColors);
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getNextHue(h, distance) {
+    h += distance;
+    h = h > 1 ? h - 1 : h;
+    return h;
+}
